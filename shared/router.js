2832b7a40d2dfaadb8fbc1bafe5a988b33f49e44
Christian Bundy <me@christianbundy.com>
2014-03-13 12:18:17 -0700

    Add post template and route

--- shared/router.js
+++ shared/router.js
@@ -4,6 +4,10 @@ Router.configure({
 
 Router.map(function () {
   "use strict";
+  this.route('post', {
+    path: '/post',
+    template: 'post'
+  });
   this.route('index', {
     path: '*',
     template: 'feed'
