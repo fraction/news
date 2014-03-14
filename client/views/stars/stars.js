3266db3f7faf7180175189f3c9bee541ea90116e
Christian Bundy <me@christianbundy.com>
2014-03-14 13:02:19 -0700

    Change star num to number
    This makes it a little bit easier to tell what's actually going on.

--- client/views/stars/stars.js
+++ client/views/stars/stars.js
@@ -4,15 +4,15 @@ var rate = function (event) {
   event.preventDefault();
   var star = event.target;
   var post = star.parentElement.parentElement.parentElement.dataset.post;
-  var starNum = event.target.dataset.num;
+  var starNumber = event.target.dataset.number;
   var oldRating = $('div[data-post=' + post + ']').attr('data-rating');
-  $('div[data-post=' + post + ']').attr('data-rating', starNum);
+  $('div[data-post=' + post + ']').attr('data-rating', starNumber);
   var setStarHighlight = function () {
     var jThis = $(this);
-    if (jThis.data("num") <= starNum) {
+    if (jThis.data("number") <= starNumber) {
       jThis
       .removeClass('fa-star-o rate-' + oldRating)
-      .addClass('fa-star rate-' + starNum);
+      .addClass('fa-star rate-' + starNumber);
     } else {
       jThis
       .removeClass('fa-star rate-' + oldRating)
@@ -20,8 +20,17 @@ var rate = function (event) {
     }
   };
   $('div[data-post=' + post + '] .star').each(setStarHighlight);
+
 };
 
 Template.stars.events({
   'click .star': rate
 });
+
+Template.stars.type = function () {
+
+};
+
+Template.stars.number = function () {
+
+};
