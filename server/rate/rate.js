ecc3c332d22d9953e4b1f697786b90b34c6e43dc
Christian Bundy <me@christianbundy.com>
2014-03-14 15:07:42 -0700

    Finish server-side rate function

--- server/rate/rate.js
+++ server/rate/rate.js
@@ -1,6 +1,25 @@
 Meteor.methods({
+  // obj.id = content _id
+  // obj.rate = score from 1-5
+
   rate : function (obj) {
     "use strict";
+    if (typeof obj === 'undefined') {
+      throw 'Must include object with content ID and rating';
+    }
+
+    if (typeof obj.id === 'undefined') {
+      throw 'ID must be included';
+    }
+
+    if (typeof obj.rate === 'undefined') {
+      throw 'Rating must be included';
+    }
+
+    if (obj.rate < 1 || obj.rate > 5) {
+      throw 'Rating must be between 1 and 5';
+    }
+
     var findUserQuery = {};
     findUserQuery[Meteor.userId()] = {
       $exists: true
@@ -9,8 +28,19 @@ Meteor.methods({
     var findUser = Ratings.findOne(findUserQuery);
     if (typeof findUser === 'undefined') {
       var insertion = {};
-      insertion[Meteor.userId()] = 'hey'
+      insertion[Meteor.userId()] = {}
       Ratings.insert(insertion);
     }
+
+    var updateRatingsQuery = {};
+    updateRatingsQuery[obj.id] = {
+      time: new Date(),
+      rate: obj.rate
+    };
+
+    Ratings.update(findUserQuery, updateRatingsQuery, {
+      upsert: true,
+      multi: false
+    });
   }
 });
