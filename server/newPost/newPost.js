516ce2bad726c47bee8103cf2df46f0fe7b77c63
Christian Bundy <me@christianbundy.com>
2014-03-14 11:47:11 -0700

    Add git logging

---
+++ server/newPost/newPost.js
@@ -0,0 +1,3 @@
+Meteor.methods({
+  log: console.log
+});
--- shared/posts.js
+++ shared/posts.js
@@ -1,2 +1,9 @@
 /* global Posts:true */
+
 Posts = new Meteor.Collection('posts');
+{
+    title:    "I'm a title!"
+    url:      "http://fraction.io/"
+    comments: "0"
+    author:   "christianbundy"
+}
