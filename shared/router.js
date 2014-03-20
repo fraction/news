40487a953be29133935fd9f84fd7ac8cbb122851
Christian Bundy <me@christianbundy.com>
2014-03-19 22:07:41 -0700

    Rewrite header and organize template
    Creates the wrapper template, changes the footer template, and revamps the header completely.

--- shared/router.js
+++ shared/router.js
@@ -6,23 +6,40 @@ Router.map(function () {
   "use strict";
   this.route('newPost', {
     path:     '/new-post',
-    template: 'newPost'
+    template: 'newPost',
+    data: function () {
+      var templateData = {
+        currentView: 'New Post',
+        posts: Posts.find({username : this.params.username})
+      };
+      return templateData;
+    }
   });
+
   this.route('index', {
     path:     '/',
-    template: 'feed'
+    template: 'feed',
+    data: function () {
+      var templateData = {
+        currentView: 'Popular',
+      };
+      return templateData;
+    }
   });
+
   this.route('user', {
     path:     'user/:username',
     template: 'user',
     data: function () {
       var templateData = {
+        currentView: this.params.username,
         username: this.params.username,
         posts: Posts.find({username : this.params.username})
       };
       return templateData;
     }
   });
+
   this.route('comments', {
     path:     'comments/:id/*',
     template: 'comments'
