b417f4ec55d9ea6759cb2f227f6e9a12c2bba31c
Christian Bundy <me@christianbundy.com>
2014-04-04 12:59:57 -0700

    Clean up function from SO and "use strict" everywhwere

--- client/views/post/post.js
+++ client/views/post/post.js
@@ -15,32 +15,32 @@ var cleanText = function (str) {
 
 // http://stackoverflow.com/a/3177838
 var timeSince = function (date) {
+  "use strict";
+  var seconds = Math.floor((new Date() - date) / 1000);
 
-    var seconds = Math.floor((new Date() - date) / 1000);
+  var interval = Math.floor(seconds / 31536000);
 
-    var interval = Math.floor(seconds / 31536000);
-
-    if (interval > 1) {
-        return interval + " years";
-    }
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
-        return interval + " months";
-    }
-    interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
-        return interval + " days";
-    }
-    interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-        return interval + " hours";
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) {
-        return interval + " minutes";
-    }
-    return Math.floor(seconds) + " seconds";
-}
+  if (interval > 1) {
+    return interval + " years";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " months";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " days";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hours";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
+};
 
 Template.post.helpers({
   commentCopy: function() {
@@ -81,6 +81,7 @@ Template.post.helpers({
     }
   },
   timeCopy: function () {
+    "use strict";
     return timeSince(this.time);
   }
 });
