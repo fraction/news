62ede9b05df2e5d293232d1a58055778655cf5df
Christian Bundy <me@christianbundy.com>
2014-04-04 12:34:31 -0700

    Add time since content was posted

--- client/views/post/post.js
+++ client/views/post/post.js
@@ -13,6 +13,35 @@ var cleanText = function (str) {
   return str.replace(/[^\w\s]/gi, '').replace(/\W/g, "-").toLowerCase();
 };
 
+// http://stackoverflow.com/a/3177838
+var timeSince = function (date) {
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+}
+
 Template.post.helpers({
   commentCopy: function() {
     "use strict";
@@ -50,5 +79,8 @@ Template.post.helpers({
     } else {
       return 'nonvoted';
     }
+  },
+  timeCopy: function () {
+    return timeSince(this.time);
   }
 });
