e50ee05d7d955a48978f2e0ab6d5edc35bc3ed8a
Christian Bundy <me@christianbundy.com>
2014-04-07 17:30:05 -0700

    Add timing to routes
    This adds timing for daily, weekly, monthly, yearly, and ever.

--- shared/router.js
+++ shared/router.js
@@ -23,7 +23,7 @@ Router.map(function () {
   this.route('home', {
     path: '/',
     action: function () {
-      Router.go('/popular');
+      Router.go('/popular/weekly');
     }
   });
 
@@ -53,16 +53,42 @@ Router.map(function () {
   });
 
   this.route('feed', {
-    path:     '/:order',
+    path:     '/:order/:time',
     template: 'feed',
     data: function () {
       var order = this.params.order.toLowerCase();
+      var start = new Date();
+      var time = this.params.time.toLowerCase();
+
+      switch (time) {
+      case 'daily':
+        start = new Date(start.setDate(start.getDate() - 1));
+        break;
+      case 'weekly':
+        start = new Date(start.setDate(start.getDate() - 7));
+        break;
+      case 'monthly':
+        start = new Date(start.setFullYear(start.getFullYear(), start.getMonth() - 1));
+        break;
+      case 'yearly':
+        start = new Date(start.setFullYear(start.getFullYear() - 1));
+        break;
+      case 'ever':
+        start = new Date(0);
+        break;
+      default:
+        Router.go('/popular/weekly');
+        break;
+      }
+
+      console.log(start);
+
       var templateData = {
         currentView: 'Home',
         isFeed: true
       };
       if (order === 'popular') {
-        var posts = Posts.find().fetch();
+        var posts = Posts.find({time: {$gte: start}}).fetch();
         var popularPosts = [];
         var pointTable = [];
         Meteor.call('countVotes', posts, function (err, data) {
@@ -82,11 +108,11 @@ Router.map(function () {
       } else if (order === 'recent') {
         templateData.sortType = 'Recent';
         templateData.sortRecent = true;
-        templateData.posts = Posts.find().fetch();
+        templateData.posts = Posts.find({time: {$gte: start}}).fetch();
       } else if (order === 'random') {
         templateData.sortType = 'Random';
         templateData.sortRandom = true;
-        templateData.posts = shuffle(Posts.find().fetch());
+        templateData.posts = shuffle(Posts.find({time: {$gte: start}}).fetch());
       }
       return templateData;
     }
