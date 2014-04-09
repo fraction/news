f6ae2984ed0319dfe1eeac77732dbced74e7e034
Christian Bundy <me@christianbundy.com>
2014-04-08 17:21:56 -0700

    Add janky RSS integration with Raptitude
    This absolutely needs to be fixed up, but it's quick-n-dirty implementation of a server-side RSS reader.

--- server/rss/rss.js
+++ server/rss/rss.js
@@ -1,9 +1,5 @@
 var queue = new PowerQueue();
-queue.run();
 
-var fp = Meteor.require('feedparser');
-
-console.log(fp);
 
 Meteor.methods({
   addRSS: function (url) {
@@ -21,4 +17,51 @@ Meteor.methods({
       });
     });
   }
+});
+
+
+var FeedParser = Meteor.require('feedparser')
+  , request = Meteor.require('request')
+  , Fiber = Meteor.require('fibers');
+
+// example
+
+
+Meteor.startup(function () {
+  var req = request('http://raptitude.com/rss')
+    , feedparser = new FeedParser();
+  req.on('error', function (error) {
+    console.log(error);
+  }).on('response', function (res) {
+    var stream = this;
+    if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
+    stream.pipe(feedparser);
+  });
+
+  feedparser.on('error', function(error) {
+    console.log('shit')
+  });
+  feedparser.on('readable', function() {
+    var stream = this, item;
+
+    while (item = stream.read()) {
+      Fiber(function() {
+        var obj = {};
+        if (typeof item.title !== 'undefined') {
+          obj.title = item.title;
+        }
+        if (typeof item.origlink !== 'undefined') {
+          obj.url = item.origlink
+        }
+
+        obj.time = new Date();
+        obj.author = 'raptitude.com';
+        obj.username = 'raptitude.com';
+        obj.comments = 0;
+
+        Posts.insert(obj);
+        console.log('');
+      }).run();
+    }
+  });
 });
