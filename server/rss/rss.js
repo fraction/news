edd0e150be42d6671b2161ea89a8f7b92655d755
Christian Bundy <me@christianbundy.com>
2014-04-08 18:06:52 -0700

    Fix problem with null RSS results

--- server/rss/rss.js
+++ server/rss/rss.js
@@ -28,11 +28,12 @@ var FeedParser = Meteor.require('feedparser')
 
 
 Meteor.startup(function () {
-  var req = request('http://raptitude.com/rss')
+  var req = request('http://theglitterguide.com/feed/')
     , feedparser = new FeedParser();
   req.on('error', function (error) {
     console.log(error);
   }).on('response', function (res) {
+    var stream = this;
     var stream = this;
     if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
     stream.pipe(feedparser);
@@ -50,13 +51,17 @@ Meteor.startup(function () {
         if (typeof item.title !== 'undefined') {
           obj.title = item.title;
         }
-        if (typeof item.origlink !== 'undefined') {
+        if (item.origlink != null) {
           obj.url = item.origlink
+        } else if (item.link != null){
+          obj.url = item.link;
         }
 
+        console.log(item.link);
+
         obj.time = new Date();
-        obj.author = 'raptitude.com';
-        obj.username = 'raptitude.com';
+        obj.author = 'theglitterguide.com';
+        obj.username = 'theglitterguide.com';
         obj.comments = 0;
 
         Posts.insert(obj);
