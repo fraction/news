1ff257d154742bb639790cd48be1aa0fb93c3dbd
Christian Bundy <me@christianbundy.com>
2014-04-15 17:45:20 -0700

    Add auto-reading from Hacker News
    Every hour, read the best ~20 posts from the past 24 hours from Hacker News.

--- server/hn/readHn.js
+++ server/hn/readHn.js
@@ -1,35 +1,46 @@
 //var queue = new PowerQueue();
 var hn = Meteor.require('hacker-news-api');
 
-Meteor.methods({
-  "readHn": function () {
-    "use strict";
-    hn.getStories(Meteor.bindEnvironment(
-      function (error, data) {
-        if (error) {
-          throw error;
-        }
-        _(data.hits).forEach(function (item) {
-          /*jshint camelcase: false */
-          var obj = {
-            oldId: parseInt(item.objectID, 10),
-            oldPoints: parseInt(item.points, 10),
-            createdAt: new Date(item.created_at),
-            site: 'hn',
-            author: item.author,
-            title: item.title,
-            url: item.url,
-            comments: parseInt(item.num_comments, 10),
-          };
+var readHn = function () {
+  "use strict";
+  console.log('Reading last 24 hours of Hacker News');
+  var now = Date.now() / 1000;
 
-          Posts.upsert({
-              oldId: obj.oldId
-          },{ $set: obj });
+  var query = 'search?tags=story&numericFilters=created_at_i>' +
+  (now - 86400) + ',created_at_i<' +
+  now;
 
-        });
-      }, function (e) {
-        throw e;
-      })
-    );
-  }
-});
+  hn.call(query, Meteor.bindEnvironment(
+    function (error, data) {
+      if (error) {
+        throw error;
+      }
+      _(data.hits).forEach(function (item) {
+        /*jshint camelcase: false */
+        var obj = {
+          oldId: parseInt(item.objectID, 10),
+          oldPoints: parseInt(item.points, 10),
+          createdAt: new Date(item.created_at),
+          site: 'hn',
+          author: item.author,
+          title: item.title,
+          url: item.url,
+          comments: parseInt(item.num_comments, 10),
+        };
+
+        Posts.upsert({
+            oldId: obj.oldId
+        },{ $set: obj });
+
+      });
+    }, function (e) {
+      throw e;
+    })
+  );
+}
+
+// read last 24 hours of hn
+readHn();
+
+// reread hn every hour
+Meteor.setInterval(readHn, 3600 * 1000)
