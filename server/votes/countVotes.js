edb05c11926c874b17ee499bf0685e6ffc1db6e1
Christian Bundy <me@christianbundy.com>
2014-04-15 22:22:53 -0700

    Add sorting count

--- server/votes/countVotes.js
+++ server/votes/countVotes.js
@@ -1,37 +1,37 @@
-Meteor.methods({
-  // obj.id = content _id
-  // obj.vote = score from -1 to 1
-  // TODO: protect against inserting vote for non-existent content
-  countVotes : function (posts) {
-    "use strict";
-    if (typeof obj !== 'undefined') {
-      new Meteor.Error(500, 'Must include posts with call');
-    }
-    var pointTable = [];
+var countVotes = function () {
+  "use strict";
 
-    var count = function (obj) {
-      var countVotesQuery = {};
-      countVotesQuery['votes.' + obj._id] = {
-        '$exists' : true
-      };
+  var voteTable = [];
 
-      var countVotesResult = Votes.find(countVotesQuery).fetch();
-      var current = 0;
-
-      _(countVotesResult).forEach(function (result) {
-        current += result.votes[obj._id].vote;
-      });
-
-      return current;
+  var count = function (obj) {
+    var countVotesQuery = {};
+    countVotesQuery['votes.' + obj._id] = {
+      '$exists' : true
     };
 
-    _(posts).forEach(function (obj) {
-      pointTable.push({
-        id: obj._id,
-        votes: count(obj)
-      });
+    var countVotesResult = Votes.find(countVotesQuery).fetch();
+    var current = 0;
+
+    _(countVotesResult).forEach(function (result) {
+      current += result.votes[obj._id].vote;
     });
 
-    return pointTable;
-  }
+    return current;
+  };
+
+  _(Posts.find().fetch()).forEach(function (obj) {
+    voteTable.push({
+      id: obj._id,
+      votes: count(obj)
+    });
+  });
+
+  _(voteTable).forEach(function(item) {
+    Posts.update({_id: item.id}, { $set: {votes: item.votes}});
+  });
+};
+
+
+Meteor.startup(function () {
+  countVotes();
 });
