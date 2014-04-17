d180964e5e30fe2c2318f0e5d20d547e9e9749e4
Christian Bundy <me@christianbundy.com>
2014-04-16 17:30:33 -0700

    Add more times to scrape HN

--- server/hn/readHn.js
+++ server/hn/readHn.js
@@ -3,46 +3,80 @@ var hn = Meteor.require('hacker-news-api');
 
 var readHn = function () {
   "use strict";
-  console.log('Reading last 24 hours of Hacker News');
+  console.log('Reading last hour, 12 hours, 24 hours, and week of Hacker News');
   var now = Date.now() / 1000;
+  var querylist = [];
 
-  var query = 'search?tags=story&numericFilters=created_at_i>' +
-  (now - 86400) + ',created_at_i<' +
-  now;
+  // past hour
+  querylist.push('search?tags=story&numericFilters=created_at_i>' +
+  (now - 60 * 60) + ',created_at_i<' +
+  now);
 
-  hn.call(query, Meteor.bindEnvironment(
-    function (error, data) {
-      if (error) {
-        throw error;
-      }
-      _(data.hits).forEach(function (item) {
-        /*jshint camelcase: false */
-        var obj = {
-          oldId: parseInt(item.objectID, 10),
-          oldPoints: parseInt(item.points, 10),
-          createdAt: new Date(item.created_at),
-          site: 'hn',
-          author: item.author,
-          title: item.title,
-          url: item.url,
-          oldComments: parseInt(item.num_comments, 10),
-        };
+  // past 12 hours
+  querylist.push('search?tags=story&numericFilters=created_at_i>' +
+  (now - 60 * 60 * 12) + ',created_at_i<' +
+  now);
 
-        Posts.upsert({
-          oldId: obj.oldId
-        },{
-          $set: obj
+  // past 24 hours
+  querylist.push('search?tags=story&numericFilters=created_at_i>' +
+  (now - 60 * 60 * 24) + ',created_at_i<' +
+  now);
+
+  // past week
+  querylist.push('search?tags=story&numericFilters=created_at_i>' +
+  (now - 60 * 60 * 24 * 7) + ',created_at_i<' +
+  now);
+
+  // past month
+  querylist.push('search?tags=story&numericFilters=created_at_i>' +
+  (now - 60 * 60 * 24 * 30) + ',created_at_i<' +
+  now);
+
+  // past year
+  querylist.push('search?tags=story&numericFilters=created_at_i>' +
+  (now - 60 * 60 * 24 * 365) + ',created_at_i<' +
+  now);
+
+  // past ever
+  querylist.push('search?tags=story&numericFilters=created_at_i>' +
+  0 + ',created_at_i<' +
+  now);
+
+  _.forEach(querylist, function (query) {
+    hn.call(query, Meteor.bindEnvironment(
+      function (error, data) {
+        if (error) {
+          throw error;
+        }
+        _(data.hits).forEach(function (item) {
+          /*jshint camelcase: false */
+          var obj = {
+            oldId: parseInt(item.objectID, 10),
+            oldPoints: parseInt(item.points, 10),
+            createdAt: new Date(item.created_at),
+            site: 'hn',
+            author: item.author,
+            title: item.title,
+            url: item.url,
+            oldComments: parseInt(item.num_comments, 10),
+          };
+
+          Posts.upsert({
+            oldId: obj.oldId
+          },{
+            $set: obj
+          });
+
         });
-
-      });
-    }, function (error) {
-      throw error;
-    })
-  );
+      }, function (error) {
+        throw error;
+      })
+    );
+  });
 };
 
 // read last 24 hours of hn
 readHn();
 
-// reread hn every hour
-Meteor.setInterval(readHn, 3600 * 1000);
+// reread hn every half hour
+Meteor.setInterval(readHn, 1800 * 1000);
