ff1fa766e34ec5f85322bc89794a3b2e7e71b2c4
Christian Bundy <me@christianbundy.com>
2014-04-23 20:32:21 -0700

    Set heat every 10 seconds
    Instead of trying to find the post heat every time the hot page is hit,
    calculate post heat every ten seconds and save it to the database.

--- server/posts/publishPosts.js
+++ server/posts/publishPosts.js
@@ -6,13 +6,9 @@ Meteor.publish('allPosts', function () {
   return result;
 });
 
-Meteor.publish('recentPosts', function (start) {
+Meteor.publish('recentPosts', function () {
   "use strict";
-  return Posts.find({
-    createdAt: {
-      $gte: start
-    }
-  },{
+  return Posts.find({}, {
     sort: {
       createdAt: -1
     },
@@ -37,78 +33,12 @@ Meteor.publish('bestPosts', function (start) {
   });
 });
 
-Meteor.publish('hotPosts', function (start) {
+Meteor.publish('hotPosts', function () {
   "use strict";
-  var allArr = Posts.find({
-    oldPoints: {
-      $gt: 1
-    },
-    createdAt: {
-      $gte: start
-    }
-  }, {
-    sort: {
-      oldPoints: -1
-    },
-    limit: 10000
-  }).fetch();
-
-  var hotTable = {};
-
-  console.log('Sorting the ' + allArr.length + ' hottest posts');
-
-  _(allArr).forEach(function (post) {
-    var secondsAgo = Math.abs((new Date()) - (new Date(post.createdAt))) / 1000;
-    var hoursAgo = secondsAgo / 60;
-    var points = (post.oldPoints - 1);
-    var decay = Math.pow(hoursAgo + 2, 1.5);
-    hotTable[post._id] = points / decay;
-  });
-
-  var sortObject = function (obj) {
-    var arr = [];
-    for (var prop in obj) {
-      if (obj.hasOwnProperty(prop)) {
-        arr.push({
-          'key': prop,
-          'value': obj[prop]
-        });
-      }
-    }
-    arr.sort(function(a, b) { return b.value - a.value; });
-    return arr; // returns array
-  };
-
-  var sorted = sortObject(hotTable);
-
-  // delete all of the old data and insert the new stuff
-  Hot.remove({}, function (err) {
-    if (err) {
-      throw err;
-    } else {
-      _.forEach(sorted, function (el) {
-        Meteor.bindEnvironment(function() {
-          Hot.insert(Posts.findOne({_id: el.key}));
-        })();
-      });
-    }
-  });
-
-  return Hot.find(
-    {},
-    {
-      limit: 50
-    }
-  );
-});
-
-
-Meteor.publish('randomPosts', function (start) {
-  "use strict";
-  return Posts.find({
-    createdAt: {$gte: start}
-  },{
+  return Posts.find({}, {
     limit: 50,
-    skip: 10
+    sort: {
+      heat: -1
+    }
   });
 });
---
+++ server/sort/setHeat.js
@@ -0,0 +1,28 @@
+var setHeat = function () {
+  var allArr = Posts.find({
+    oldPoints: {
+      $gt: 1
+    }
+  }).fetch();
+
+  console.log('Calculating post heat');
+
+  _(allArr).forEach(function (post) {
+    var secondsAgo = Math.abs((new Date()) - (new Date(post.createdAt))) / 1000;
+    var hoursAgo = secondsAgo / 60;
+    var points = (post.oldPoints - 1);
+    var decay = Math.pow(hoursAgo + 2, 1.5);
+    Posts.update({
+      _id: post._id
+    },{
+      $set: {
+        heat: points / decay
+      }
+    });
+  });
+};
+
+Meteor.startup(setHeat);
+
+// run every 10 seconds
+Meteor.setInterval(setHeat, 10 * 1000);
