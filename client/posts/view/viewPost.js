852bb72b36ec471c90f18c7a7d79731fdca6c0f9
Christian Bundy <me@christianbundy.com>
2014-04-29 20:07:53 -0700

    Use icons instead of text where possible

--- client/posts/view/viewPost.js
+++ client/posts/view/viewPost.js
@@ -10,34 +10,30 @@ var timeSince = function (date) {
   "use strict";
 
   var timeString = function (int, str) {
-    var suffix = '';
-    if (Math.floor(int) !== 1) {
-      suffix = 's';
-    }
-    return int + ' ' + str + suffix;
+    return int + str;
   };
 
   var seconds = Math.floor((new Date() - date) / 1000);
   var interval = Math.floor(seconds / 31536000);
 
   if (interval > 0) {
-    return timeString(interval, 'year');
+    return timeString(interval, 'y');
   }
   interval = Math.floor(seconds / 2592000);
   if (interval > 0) {
-    return timeString(interval, 'month');
+    return timeString(interval, 'mo');
   }
   interval = Math.floor(seconds / 86400);
   if (interval > 0) {
-    return timeString(interval, 'day');
+    return timeString(interval, 'd');
   }
   interval = Math.floor(seconds / 3600);
   if (interval > 0) {
-    return timeString(interval, 'hour');
+    return timeString(interval, 'h');
   }
   interval = Math.floor(seconds / 60);
   if (interval > 0) {
-    return timeString(interval, 'minute');
+    return timeString(interval, 'min');
   }
   return timeString(interval, 'second');
 };
