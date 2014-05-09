a6e446da1961d8a079aaf8688b73800b33bcfc86
Christian Bundy <me@christianbundy.com>
2014-05-09 16:53:01 -0700

    Add a semicolon because I'm stupid

--- server/posts/setPostHeat.js
+++ server/posts/setPostHeat.js
@@ -11,7 +11,7 @@ require('posts', function (Posts) {
     console.log('Setting post heat for ' + allArr.length + ' posts');
     _(allArr).forEach(function (post) {
       var now = new Date();
-      var secondsAgo = Math.abs(now - (new Date(post.createdAt))) / 1000
+      var secondsAgo = Math.abs(now - (new Date(post.createdAt))) / 1000;
       var hoursAgo = secondsAgo / 60 / 60;
       var points = (post.oldPoints - 1);
       var decay = Math.pow(hoursAgo + 2, 1.5);
