3d81bf9e76d89c1b6e5f1146c3c5cff72e35a6bd
Christian Bundy <me@christianbundy.com>
2014-05-20 13:51:41 -0700

    Move votes to package fraction-votes

--- packages/fraction-votes/package.js
+++ packages/fraction-votes/package.js
@@ -7,7 +7,13 @@ Package.describe({
 Package.on_use(function (api) {
   var both = ['client', 'server'];
 
+  api.use('fraction-posts', both);
+
   api.add_files('namespace.js', both);
+
+  api.add_files('lib/count.next.js');
+  api.add_files('lib/publish.next.js');
+  api.add_files('lib/methods.next.js');
 
   api.export('Votes', both);
 });
